178a13a5ad9eaa78afcc277dd463139baefce40f
Laurent <ly.testud@outlook.com>
2024-06-06 12:00:07 -0700

    Styling the TextBox control

--- TextoCopier/TextoCopier/Workflow/Groups/NewGroupViewModel.cs
+++ TextoCopier/TextoCopier/Workflow/Groups/NewGroupViewModel.cs
@@ -8,10 +8,18 @@ public sealed class NewGroupViewModel : Bindable<NewGroupView>
     {
         this.messenger = ApplicationBase.GetRequiredService<IMessenger>();
         this.CloseCommand = new Command(this.OnClose);
+        this.SaveCommand = new Command(this.OnSave);
+    }
+
+    private void OnSave(object? _)
+    {
+        this.OnClose(_); 
     }
 
     private void OnClose(object? _)
         => this.messenger.Publish(new ViewActivationMessage(ViewActivationMessage.StaticView.GoBack));
 
     public ICommand CloseCommand { get => this.Get<ICommand>()!; set => this.Set(value); }
+
+    public ICommand SaveCommand { get => this.Get<ICommand>()!; set => this.Set(value); }
 }
