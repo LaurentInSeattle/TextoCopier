05ba642d9004e77a5b1c53a587334fdd69afadb2
Laurent <ly.testud@outlook.com>
2024-07-30 13:37:31 -0700

    Experiments

---
+++ TextoCopier/Invasion/Controls/ZoomAndPan_IS/ZoomAndPanController.xaml.cs
@@ -0,0 +1,449 @@
+﻿
+namespace ISC.Melody.Controls
+{
+    using System;
+    using System.Diagnostics;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using ISC.Melody.Framework;
+
+    /// <summary>
+    /// Defines the current state of the mouse handling logic.
+    /// </summary>
+    public enum MouseHandlingMode
+    {
+        /// <summary>
+        /// Not in any special mode.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// The user is left-mouse-button-dragging to pan the viewport.
+        /// </summary>
+        Panning,
+
+        /// <summary>
+        /// The user is holding down shift and left-clicking or right-clicking to zoom in or out.
+        /// </summary>
+        Zooming,
+
+        /// <summary>
+        /// The user is holding down shift and left-mouse-button-dragging to select a region to zoom to.
+        /// </summary>
+        DragZooming,
+    }
+
+    /// <summary>
+    /// Interaction logic for ZoomAndPanController.xaml
+    /// </summary>
+    public partial class ZoomAndPanController : UserControl, IZoomAndPanController
+    {
+        private ZoomAndPanControl zoomAndPanControl;
+        public ZoomAndPanControl ZoomAndPanControl
+        {
+            get { return zoomAndPanControl; }
+            set
+            {
+                zoomAndPanControl = value;
+                zoomAndPanControl.Controller = this;
+            }
+        }
+
+        IInputElement ControlledContent { get { return ZoomAndPanControl.Content as IInputElement; } }
+
+        /// <summary>
+        /// Specifies the current state of the mouse handling logic.
+        /// </summary>
+        private MouseHandlingMode mouseHandlingMode = MouseHandlingMode.None;
+
+        private MouseHandlingMode MouseHandlingMode
+        {
+            get { return mouseHandlingMode; }
+            set
+            {
+                if (mouseHandlingMode != value)
+                {
+                    mouseHandlingMode = value;
+                    // Debug.WriteLine("Mouse: " + mouseHandlingMode.ToString());
+                }
+            }
+        }
+
+        /// <summary>
+        /// The point that was clicked relative to the ZoomAndPanControl.
+        /// </summary>
+        private Point origZoomAndPanControlMouseDownPoint;
+
+        /// <summary>
+        /// The point that was clicked relative to the content that is contained within the ZoomAndPanControl.
+        /// </summary>
+        private Point origContentMouseDownPoint;
+
+        /// <summary>
+        /// Records which mouse button clicked during mouse dragging.
+        /// </summary>
+        private MouseButton mouseButtonDown;
+
+        /// <summary>
+        /// Saves the previous zoom rectangle, pressing the backspace key jumps back to this zoom rectangle.
+        /// </summary>
+        private Rect prevZoomRect;
+
+        /// <summary>
+        /// Save the previous content scale, pressing the backspace key jumps back to this scale.
+        /// </summary>
+        private double prevZoomScale;
+
+        /// <summary>
+        /// Set to 'true' when the previous zoom rect is saved.
+        /// </summary>
+        private bool prevZoomRectSet = false;
+
+        private Canvas dragZoomCanvas;
+        private Border dragZoomBorder;
+
+        public ZoomAndPanController()
+        {
+            dragZoomBorder = new Border()
+            {
+                BorderBrush = Brushes.Black,
+                BorderThickness = new Thickness(1),
+                Background = Brushes.Silver,
+                CornerRadius = new CornerRadius(1),
+                Opacity = 0
+            };
+
+            dragZoomCanvas = new Canvas()
+            {
+                Visibility = System.Windows.Visibility.Collapsed,
+            };
+
+            dragZoomCanvas.Children.Add(dragZoomBorder);
+
+            InitializeComponent();
+            DataContext = this; 
+        }
+
+
+        /// <summary>
+        /// The 'ZoomIn' command (bound to the plus key) was executed.
+        /// </summary>
+        private void ZoomIn_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            ZoomIn(new Point(ZoomAndPanControl.ContentZoomFocusX, ZoomAndPanControl.ContentZoomFocusY));
+        }
+
+        /// <summary>
+        /// The 'ZoomOut' command (bound to the minus key) was executed.
+        /// </summary>
+        private void ZoomOut_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            ZoomOut(new Point(ZoomAndPanControl.ContentZoomFocusX, ZoomAndPanControl.ContentZoomFocusY));
+        }
+
+        /// <summary>
+        /// The 'JumpBackToPrevZoom' command was executed.
+        /// </summary>
+        private void JumpBackToPrevZoom_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            JumpBackToPrevZoom();
+        }
+
+        /// <summary>
+        /// Determines whether the 'JumpBackToPrevZoom' command can be executed.
+        /// </summary>
+        private void JumpBackToPrevZoom_CanExecuted(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = prevZoomRectSet;
+        }
+
+        /// <summary>
+        /// The 'Fill' command was executed.
+        /// </summary>
+        private void Fill_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            SavePrevZoomRect();
+
+            ZoomAndPanControl.AnimatedScaleToFit();
+        }
+
+        /// <summary>
+        /// The 'OneHundredPercent' command was executed.
+        /// </summary>
+        private void OneHundredPercent_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            SavePrevZoomRect();
+
+            ZoomAndPanControl.AnimatedZoomTo(1.0);
+        }
+
+        /// <summary>
+        /// Jump back to the previous zoom level.
+        /// </summary>
+        private void JumpBackToPrevZoom()
+        {
+            ZoomAndPanControl.AnimatedZoomTo(prevZoomScale, prevZoomRect);
+
+            ClearPrevZoomRect();
+        }
+
+        /// <summary>
+        /// Zoom the viewport out, centering on the specified point (in content coordinates).
+        /// </summary>
+        private void ZoomOut(Point contentZoomCenter)
+        {
+            ZoomAndPanControl.ZoomAboutPoint(ZoomAndPanControl.ContentScale - 0.1, contentZoomCenter);
+        }
+
+        /// <summary>
+        /// Zoom the viewport in, centering on the specified point (in content coordinates).
+        /// </summary>
+        private void ZoomIn(Point contentZoomCenter)
+        {
+            ZoomAndPanControl.ZoomAboutPoint(ZoomAndPanControl.ContentScale + 0.1, contentZoomCenter);
+        }
+
+        #region Drag Zoom Rectangle
+
+        /// <summary>
+        /// Initialise the rectangle that the user is dragging out.
+        /// </summary>
+        private void InitDragZoomRect(Point pt1, Point pt2)
+        {
+            SetDragZoomRect(pt1, pt2);
+
+            dragZoomCanvas.Visibility = Visibility.Visible;
+            dragZoomBorder.Opacity = 0.5;
+        }
+
+        /// <summary>
+        /// Update the position and size of the rectangle that user is dragging out.
+        /// </summary>
+        private void SetDragZoomRect(Point pt1, Point pt2)
+        {
+            double x, y, width, height;
+
+            // Determine x,y,width and height of the rect inverting the points if necessary.
+            if (pt2.X < pt1.X)
+            {
+                x = pt2.X;
+                width = pt1.X - pt2.X;
+            }
+            else
+            {
+                x = pt1.X;
+                width = pt2.X - pt1.X;
+            }
+
+            if (pt2.Y < pt1.Y)
+            {
+                y = pt2.Y;
+                height = pt1.Y - pt2.Y;
+            }
+            else
+            {
+                y = pt1.Y;
+                height = pt2.Y - pt1.Y;
+            }
+
+            // Update the coordinates of the rectangle that is being dragged out by the user.
+            // The we offset and rescale to convert from content coordinates.
+            Canvas.SetLeft(dragZoomBorder, x);
+            Canvas.SetTop(dragZoomBorder, y);
+            dragZoomBorder.Width = width;
+            dragZoomBorder.Height = height;
+        }
+
+        /// <summary>
+        /// When the user has finished dragging out the rectangle the zoom operation is applied.
+        /// </summary>
+        private void ApplyDragZoomRect()
+        {
+            // Record the previous zoom level, so that we can jump back to it when the backspace key is pressed.
+            //
+            SavePrevZoomRect();
+
+            // Retreive the rectangle that the user draggged out and zoom in on it.
+            //
+            double contentX = Canvas.GetLeft(dragZoomBorder);
+            double contentY = Canvas.GetTop(dragZoomBorder);
+            double contentWidth = dragZoomBorder.Width;
+            double contentHeight = dragZoomBorder.Height;
+            ZoomAndPanControl.AnimatedZoomTo(new Rect(contentX, contentY, contentWidth, contentHeight));
+
+            FadeOutDragZoomRect();
+        }
+
+        //
+        // Fade out the drag zoom rectangle.
+        //
+        private void FadeOutDragZoomRect()
+        {
+            AnimationHelper.StartAnimation(dragZoomBorder, Border.OpacityProperty, 0.0, 0.1,
+                delegate(object sender, EventArgs e)
+                {
+                    dragZoomCanvas.Visibility = Visibility.Collapsed;
+                });
+        }
+
+        // Record the previous zoom level, so that we can jump back to it when the backspace key is pressed.
+        //
+        private void SavePrevZoomRect()
+        {
+            prevZoomRect = new Rect(ZoomAndPanControl.ContentOffsetX, ZoomAndPanControl.ContentOffsetY, ZoomAndPanControl.ContentViewportWidth, ZoomAndPanControl.ContentViewportHeight);
+            prevZoomScale = ZoomAndPanControl.ContentScale;
+            prevZoomRectSet = true;
+        }
+
+        /// <summary>
+        /// Clear the memory of the previous zoom level.
+        /// </summary>
+        private void ClearPrevZoomRect()
+        {
+            prevZoomRectSet = false;
+        }
+
+
+        #endregion
+
+        #region Mouse Event Handling
+
+        public void OnLostMouseCapture(object sender, MouseEventArgs e)
+        {
+            ZoomAndPanControl.ReleaseMouseCapture();
+            MouseHandlingMode = MouseHandlingMode.None;
+            e.Handled = true;
+        }
+
+        public void OnMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if ((Keyboard.Modifiers & ModifierKeys.Shift) == 0)
+            {
+                Point doubleClickPoint = e.GetPosition(ControlledContent);
+                ZoomAndPanControl.AnimatedSnapTo(doubleClickPoint);
+            }
+        }
+
+        public void OnMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            e.Handled = true;
+
+            if (e.Delta > 0)
+            {
+                Point curContentMousePoint = e.GetPosition(ControlledContent);
+                ZoomIn(curContentMousePoint);
+            }
+            else if (e.Delta < 0)
+            {
+                Point curContentMousePoint = e.GetPosition(ControlledContent);
+                ZoomOut(curContentMousePoint);
+            }
+        }
+
+        public void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (MouseHandlingMode == MouseHandlingMode.Panning)
+            {
+                // The user is left-dragging the mouse.
+                // Pan the viewport by the appropriate amount.
+                Point curContentMousePoint = e.GetPosition(ControlledContent);
+                Vector dragOffset = curContentMousePoint - origContentMouseDownPoint;
+                ZoomAndPanControl.ContentOffsetX -= dragOffset.X;
+                ZoomAndPanControl.ContentOffsetY -= dragOffset.Y;
+
+                e.Handled = true;
+            }
+            else if (MouseHandlingMode == MouseHandlingMode.Zooming)
+            {
+                Point curZoomAndPanControlMousePoint = e.GetPosition(ZoomAndPanControl);
+                Vector dragOffset = curZoomAndPanControlMousePoint - origZoomAndPanControlMouseDownPoint;
+                double dragThreshold = 10;
+                if (mouseButtonDown == MouseButton.Left &&
+                    (Math.Abs(dragOffset.X) > dragThreshold ||
+                     Math.Abs(dragOffset.Y) > dragThreshold))
+                {
+                    // When Shift + left-down zooming mode and the user drags beyond the drag threshold,
+                    // initiate drag zooming mode where the user can drag out a rectangle to select the area
+                    // to zoom in on.
+                    MouseHandlingMode = MouseHandlingMode.DragZooming;
+                    Point curContentMousePoint = e.GetPosition(ControlledContent);
+                    InitDragZoomRect(origContentMouseDownPoint, curContentMousePoint);
+                }
+
+                e.Handled = true;
+            }
+            else if (MouseHandlingMode == MouseHandlingMode.DragZooming)
+            {
+                // When in drag zooming mode continously update the position of the rectangle
+                // that the user is dragging out.
+                Point curContentMousePoint = e.GetPosition(ControlledContent);
+                SetDragZoomRect(origContentMouseDownPoint, curContentMousePoint);
+
+                e.Handled = true;
+            }
+        }
+
+        public void OnMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (MouseHandlingMode != MouseHandlingMode.None)
+            {
+                if (MouseHandlingMode == MouseHandlingMode.Zooming)
+                {
+                    if (mouseButtonDown == MouseButton.Left)
+                    {
+                        // Shift + left-click zooms in on the content.
+                        ZoomIn(origContentMouseDownPoint);
+                    }
+                    else if (mouseButtonDown == MouseButton.Right)
+                    {
+                        // Shift + right-click zooms out from the content.
+                        ZoomOut(origContentMouseDownPoint);
+                    }
+                }
+                else if (MouseHandlingMode == MouseHandlingMode.DragZooming)
+                {
+                    // When drag-zooming has finished we zoom in on the rectangle that was highlighted by the user.
+                    ApplyDragZoomRect();
+                }
+            }
+
+            ZoomAndPanControl.ReleaseMouseCapture();
+            MouseHandlingMode = MouseHandlingMode.None;
+            e.Handled = true;
+        }
+
+        public void OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ControlledContent.Focus();
+            Keyboard.Focus(ControlledContent);
+
+            mouseButtonDown = e.ChangedButton;
+            origZoomAndPanControlMouseDownPoint = e.GetPosition(ZoomAndPanControl);
+            origContentMouseDownPoint = e.GetPosition(ControlledContent);
+
+            if ((Keyboard.Modifiers & ModifierKeys.Shift) != 0 &&
+                (e.ChangedButton == MouseButton.Left ||
+                 e.ChangedButton == MouseButton.Right))
+            {
+                // Shift + left- or right-down initiates zooming mode.
+                MouseHandlingMode = MouseHandlingMode.Zooming;
+            }
+            else if (mouseButtonDown == MouseButton.Left)
+            {
+                // Just a plain old left-down initiates panning mode.
+                MouseHandlingMode = MouseHandlingMode.Panning;
+            }
+
+            if (MouseHandlingMode != MouseHandlingMode.None)
+            {
+                // Capture the mouse so that we eventually receive the mouse up event.
+                ZoomAndPanControl.CaptureMouse();
+                e.Handled = true;
+            }
+        }
+
+        #endregion
+    }
+}
--- TextoCopier/Invasion/GlobalUsings.cs
+++ TextoCopier/Invasion/GlobalUsings.cs
@@ -68,6 +68,8 @@ global using Lyt.Avalonia.Persistence;
 
 #endregion Framework 
 
+global using Avalonia.Controls.PanAndZoom;
+
 global using Lyt.Invasion.Converters;
 global using Lyt.Invasion.Messaging;
 global using Lyt.Invasion.Model;
