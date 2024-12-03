b4c10d8622d734b502320d4a2be84eef83a1fce1
Laurent <ly.testud@outlook.com>
2024-12-03 09:53:02 -0800

    Fix for startup crash

--- TextoCopier/TextoCopier.Desktop/Program.cs
+++ TextoCopier/TextoCopier.Desktop/Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Diagnostics;
 using Avalonia;
-using Avalonia.Threading;
 
 #pragma warning disable IDE0130 // Namespace does not match folder structure
+
 namespace Lyt.TextoCopier.Desktop;
 
 class Program
@@ -13,23 +12,7 @@ class Program
         => BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
 
     public static AppBuilder BuildAvaloniaApp()
-    {
-        var builder = 
-            AppBuilder.Configure<App>().UsePlatformDetect().WithInterFont().LogToTrace();
-        //Dispatcher.UIThread.ShutdownStarted += UIThread_ShutdownStarted;
-        //Dispatcher.UIThread.UnhandledException += UIThread_UnhandledException;
-        return builder;
-    }
-
-    private static void UIThread_UnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
-    {
-        if (Debugger.IsAttached) { Debugger.Break(); }
-    }
-
-    private static void UIThread_ShutdownStarted(object? sender, EventArgs e)
-    {
-        if (Debugger.IsAttached) { Debugger.Break(); }
-    } 
+        => AppBuilder.Configure<App>().UsePlatformDetect().WithInterFont().LogToTrace();
 }
 
 #pragma warning restore IDE0130 
